cf9074e653b3f283e81d7ba248b7d55f1fe013de
weipa7 <hajime0578@gmail.com>
2022-03-10 22:06:27 +0900

    CRUD messages

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', 'App\Http\Controllers\MessagesController@index');
+
+Route::resource('messages', 'App\Http\Controllers\MessagesController');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +18,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// // メッセージ個別詳細ページ表示
+// Route::get('messages/{id}', 'App\Http\Controllers\MessagesController@show');
+
+// //メッセージの新規登録処理（新規登録画面は表示しない）
+// Route::post('messages', 'App\Http\Controllers\MessagesController@store');
+
+// // メッセージの更新処理（編集画面は表示しない）
+// Route::put('messages/{id}', 'MessagesController@update');
+
+// // メッセージの削除処理
+// Route::delete('messages/{id}', 'MessagesController@destroy');
+
+// //一覧画面表示
+// Route::get('messages', 'App\Http\Controllers\MessagesController@index')->name('messages.index');
+
+// // 新規登録画面表示（フォーム）
+// Route::get('messages/create', 'App\Http\Controllers\MessagesController@create')->name('messages.create');
+
+// // 更新登録画面表示（フォーム）
+// Route::get('messages/{id}/edit', 'MessagesController@edit')->name('messages.edit');
